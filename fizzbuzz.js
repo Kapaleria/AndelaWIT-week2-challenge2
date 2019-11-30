2106b59c8b728f4764900a241ab0061d4889c122
Kapaleria <38499454+Kapaleria@users.noreply.github.com>
2019-11-30 21:01:03 +0300

    Add files via upload
    Initial commit which failed all tests

---
+++ fizzbuzz.js
@@ -0,0 +1,18 @@
+
+function fizzBuzz(list1, list2) {
+var sumOfArrayLengths=list1.length + list2.length;
+    
+    if(sumOfArrayLengths%3 == 0 && sumOfArrayLengths%5!== 0 ){
+        return "Fizz";
+    }
+    else if(sumOfArrayLengths%5 == 0 && sumOfArrayLengths%3!== 0){
+        return "Buzz";
+    }
+    else if(sumOfArrayLengths %3 == 0 && sumOfArrayLengths%5== 0 ){
+        return "Fizzbuzz" ;
+    }
+    else if(sumOfArrayLengths%3!== 0 && sumOfArrayLengths%5 !== 0){
+        return sumOfArrayLengths ;
+    }
+}
+module.exports = fizzBuzz;
